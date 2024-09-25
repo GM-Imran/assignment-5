86360edb73d8b9ac922ad20365cfb38c1d1ce565
programminghero1 <mdimran813903@gmail.com>
2024-09-25 12:02:28 +0600

    done

--- js/login.js
+++ js/login.js
@@ -75,3 +75,27 @@ document.getElementById('btn-donate-now-3')
         }
 
     })
+
+
+
+
+
+    function updateLocalDateTime() {
+        const now = new Date();
+        const options = { 
+            year: 'numeric', 
+            month: '2-digit', 
+            day: '2-digit', 
+            hour: '2-digit', 
+            minute: '2-digit', 
+            second: '2-digit', 
+            hour12: true 
+        };
+        const datetimeString = now.toLocaleString([], options);
+        document.getElementById('local-datetime').innerText = datetimeString;
+    }
+
+        // Update the date and time every second
+        setInterval(updateLocalDateTime, 1000);
+        updateLocalDateTime(); // Initial call
+
